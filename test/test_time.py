d04d3e2a43a08eb96f49f204e2ce2e818d26b87f
Виктор Федоров <34129775+sar-bc@users.noreply.github.com>
2024-12-08 23:32:43 +0400

    Delete test directory

--- test/test_time.py
+++
@@ -1,7 +0,0 @@
-from datetime import date
-
-last_date = date(2024, 11, 19)
-current_date = date(2024, 11, 19)
-
-print(f"last.date: {last_date}, current_date: {current_date}")
-print(f"Are they equal? {last_date == current_date}")
